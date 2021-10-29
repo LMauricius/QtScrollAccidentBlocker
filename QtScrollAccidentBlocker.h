18522e1e452fb048f209a2cea6d3b5d1c605e4a9
Mauricius <mauriciosmit100@gmail.com>
2021-10-30 00:28:33 +0200

    Added comments

--- QtScrollAccidentBlocker.h
+++ QtScrollAccidentBlocker.h
@@ -4,26 +4,30 @@
 #include <QObject>
 #include <QMainWindow>
 
+/*
+An event filter that prevents accidental changes when scrolling over value-holding widgets.
+If the user scrolls over a widget that responds to wheel events,
+the widget that the user scrolled in before will receive the event instead.
+Currently works on QAbstractSlider, QComboBox, QTabBar, QAbstractSpinBox, QAbstractScrollArea, QScrollBar.
+
+To use, simply install QtScrollAccidentBlocker as an event filter of QApplication,
+and call QtScrollAccidentBlocker::enable().
+*/
 class QtScrollAccidentBlocker : public QObject
 {
     Q_OBJECT
 public:
     explicit QtScrollAccidentBlocker(QObject *parent = nullptr);
 
-    bool eventFilter(QObject *watched, QEvent *event) override;
-
-    // Returns whether the widget is derived from a scrollable class AND if it can be scrolled further in the delta's direction
-    bool isWidgetScrollable(const QWidget* w, QPoint angleDelta);
-    // Finds the first scrollable widget in w's ancestry (including w) or returns nullptr if no scrollable widget can be found
-    QWidget* getFirstScrollableInAncestry(QWidget* w, QPoint angleDelta);
-
     // Call this to enable event redirection. Returns whether successful
     bool enable();
 
-    // Returns whether successful
+    // Call this to disable event redirection. Returns whether successful
     bool disable();
 
-signals:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+    bool isWidgetScrollable(const QWidget* w, QPoint angleDelta);// Returns whether the widget is derived from a scrollable class AND if it can be scrolled further in the delta's direction
+    QWidget* getFirstScrollableInAncestry(QWidget* w, QPoint angleDelta);// Finds the first scrollable widget in w's ancestry (including w) or returns nullptr if no scrollable widget can be found
 
 protected:
     QWidget *mScrollActiveWidget;
